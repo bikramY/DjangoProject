e1d6639e828c2ef3eac9f290727cd2cbd598cbdf
bikramY <iambikram.ydv@gmail.com>
2022-03-09 23:46:37 +0545

    version 1

---
+++ Home/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, HttpResponse
+from Home.models import Contact
+from datetime import datetime
+# Create your views here.
+def index(request):
+    # return HttpResponse("Hello this is a home page")
+    return render(request,'index.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def contact(request):
+    if request.method=='POST':
+        name=request.POST.get('name')
+        address=request.POST.get('address')
+        email=request.POST.get('email')
+        issue=request.POST.get('issue')
+        date= datetime.today()
+        contact= Contact(name=name,address=address,email=email,issue=issue,date=date)
+        contact.save()
+        
+
+    return render(request,'contact.html')
+
+
